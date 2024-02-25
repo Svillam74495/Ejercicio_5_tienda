417fe0f75798f4b09b87b7696fc0126a3ee4fba2
Svillam74495 <104543238+Svillam74495@users.noreply.github.com>
2024-02-25 18:02:35 -0500

    Add files via upload

---
+++ producto.java
@@ -0,0 +1,21 @@
+public class producto {
+
+	private int codigo;
+
+	private string nombre;
+
+	private string tipo;
+
+	private Date fecha_expiracion;
+
+	private string nombre_fabricante;
+
+	private int cantidad_inventario;
+
+	private int precio_inventario;
+
+	public void almacenar() {
+
+	}
+
+}
